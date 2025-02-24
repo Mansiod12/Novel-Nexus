c94442ad2a321300fe80c8681ba4bbcc7bc6818f
Mansi Odedra <odedrashanti76@gmail.com>
2025-02-24 11:27:06 +0530

    .

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL, // Allow frontend requests
+    credentials: true, // If using cookies or authentication
+  }));
 
 // Routes
 app.use("/api/v1", user);
